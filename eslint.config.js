4d322bbfb8ec0d1136ac7626ff23ef50f6b2fdc4
AdamS <adams.manchester2019a@gmail.com>
2025-04-27 13:05:16 +0100

    Eslint fix and setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,9 @@
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintPluginAstro from "eslint-plugin-astro";
 export default [
   // add more generic rule sets here, such as:
   // js.configs.recommended,
-  ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["all"], // In CommonJS, the `flat/` prefix is required.
   {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    ***REMOVED***
-  ***REMOVED***
+    rules: {***REMOVED***,
+  ***REMOVED***,
 ];
