622d3b8d30ecb01711d9f046874f2c17e253630e
AdamS <adams.manchester2019a@gmail.com>
2025-04-28 21:47:51 +0100

    Changed Nav Bar Icon

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { open: 'never' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -73,7 +75,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run preview',
-    timeout: 60 * 1000,
+    timeout: 120 * 1000,
     reuseExistingServer: true,
     port: 4321
   }
