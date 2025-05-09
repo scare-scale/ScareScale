efd219cc0bc5bedd44033583594df2be25c3e62f
AdamS <adams.manchester2019a@gmail.com>
2025-05-09 18:28:11 +0100

    Refactor movie data handling by removing direct collection calls

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,7 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, getCollection, z } from "astro:content";
+import { calculateOverallRating, fearLevelText } from "./utils/scoreUtils";
+
+const POSTER_BASE_URL = "https://www.themoviedb.org/t/p/w300_and_h450_bestv2";
 
 const movieCollection = defineCollection({
   schema: z.object({
@@ -22,6 +25,38 @@ const pageCollection = defineCollection({
     description: z.string().optional(),
   }),
 });
+
+const getMovies = async () => {
+  const movieEntries = await getCollection("movie");
+  return movieEntries.map((movie) => {
+    const posterUrl = `${POSTER_BASE_URL}${movie.data.tmdbPosterId}`;
+
+    const scareScaleRating = calculateOverallRating(movie.data.categoryRatings);
+    const scareScaleText = fearLevelText(scareScaleRating);
+    const parsedDate = new Date(String(movie.data.releaseDate));
+    const formattedDate = parsedDate.toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+    return {
+      ...movie,
+      posterUrl,
+      scareScaleRating,
+      scareScaleText,
+      formattedDate,
+      parsedDate,
+      releaseYear: parsedDate.getFullYear()
+    };
+  });
+};
+
+export const allMovies = await getMovies();
+
+export const upcomingMovies = (await getMovies())
+    .filter((movie) => movie.parsedDate > new Date())
+    .sort((a, b) => a.parsedDate.getTime() - b.parsedDate.getTime());
 
 export const collections = {
   movie: movieCollection,
