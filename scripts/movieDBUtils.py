bbd93483710ac6f317a7640c85d6c34f634b956a
AdamS <adams.manchester2019a@gmail.com>
2025-05-09 20:59:00 +0100

    Rated additional movies

--- scripts/movieDBUtils.py
+++ scripts/movieDBUtils.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import re
+from tqdm import tqdm
 
 def get_tmdb_headers():
     """Retrieve TMDB API headers."""
@@ -44,18 +45,29 @@ def fetch_horror_movies(start_date, end_date, min_popularity=50):
     }
 
     all_movies = []
-    while True:
-        response = requests.get(url, headers=headers, params=params)
-        if response.status_code == 200:
-            data = response.json()
-            filtered_movies = [movie for movie in data.get("results", []) if movie.get("popularity", 0) >= min_popularity]
-            all_movies.extend(filtered_movies)
-            if data.get("page") >= data.get("total_pages"):
-                break
-            params["page"] += 1
-        else:
-            print(f"Error: Unable to fetch movies. Status code {response.status_code}")
-            break
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        total_results = data.get("total_results", 0)
+        with tqdm(total=total_results, desc="Fetching movies") as pbar:
+            total_pages = data.get("total_pages", 1)
+            for page in range(1, total_pages + 1):
+                params["page"] = page
+                response = requests.get(url, headers=headers, params=params)
+                if response.status_code == 200:
+                    data = response.json()
+                    filtered_movies = [
+                        movie for movie in data.get("results", [])
+                        if movie.get("popularity", 0) >= min_popularity and
+                        not any(genre_id in [35, 10751] for genre_id in movie.get("genre_ids", []))  # Exclude Comedy (35) and Family (10751)
+                    ]
+                    all_movies.extend(filtered_movies)
+                    pbar.update(len(data.get("results", [])))
+                else:
+                    print(f"Error: Unable to fetch movies on page {page}. Status code {response.status_code}")
+                    break
+    else:
+        print(f"Error: Unable to fetch movies. Status code {response.status_code}")
 
     return all_movies
 
--- src/content/movie/the-invisible-man-2020.md
+++ src/content/movie/the-invisible-man-2020.md
@@ -4,10 +4,10 @@ tmdbId: "570670"
 tmdbPosterId: "/5EufsDwXdY2CVttYOk2WtYhgKpa.jpg"
 releaseDate: "2020-02-26"
 categoryRatings:
-    gore: 0
-    creepy: 0
-    jumpscares: 0
-    suspense: 0
-    psychological: 0
+    gore: 2
+    creepy: 5
+    jumpscares: 5
+    suspense: 7
+    psychological: 3
 ---
 When Cecilia's abusive ex takes his own life and leaves her his fortune, she suspects his death was a hoax. As a series of coincidences turn lethal, Cecilia works to prove that she is being hunted by someone nobody can see.
