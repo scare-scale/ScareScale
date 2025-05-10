e43995cb37bea633a594dc457dcd7e1fcc5d2247
AdamS <adams.manchester2019a@gmail.com>
2025-05-10 20:30:55 +0100

    Re-added quotes arround movie names

--- scripts/movieDBUtils.py
+++ scripts/movieDBUtils.py
@@ -87,7 +87,7 @@ def save_as_md(movie_data):
 
         # Update existing values or add new ones
         updated_content = {
-            "name": movie_data['name'],  # Use double quotes for the outer string
+            "name": f"\"{movie_data['name']}\"",  # Use double quotes for the outer string
             "tmdbId": f"\"{movie_data['tmdbId']}\"",
             "tmdbPosterId": movie_data['tmdbPosterId'],
             "releaseDate": movie_data['releaseDate'],  # Output as date object
