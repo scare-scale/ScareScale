aee8ad398c94ada7a605d69b4071b04c60498a9a
AdamS <adams.manchester2019a@gmail.com>
2025-08-17 09:41:57 +0100

    Fixed tests

--- acceptance-tests/movie.spec.ts
+++ acceptance-tests/movie.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("/movie/heart-eyes-2025");
-  await expect(page).toHaveTitle("Scare Scale - Heart Eyes Review & Scare Ratings");
+  await expect(page).toHaveTitle("Heart Eyes (2025) Review & Scare Scale Rating");
 });
 
 test("movie details", async ({ page }) => {
