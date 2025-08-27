a823a50bad740a27f3f128595e9244d69729e636
AdamS <adams.manchester2019a@gmail.com>
2025-08-27 18:27:52 +0100

    Updated similar movies

--- src/utils/comparisonUtils.ts
+++ src/utils/comparisonUtils.ts
@@ -1,3 +1,5 @@
+const ignoredWords = new Set(["the", "and", "it", "of", "a", "an", "in", "on", "to", "with", "for", "at", "by", "from"]);
+
 const isSameSlug = (a, b): boolean => a.slug === b.slug;
 
 const hasSameTopCategories = (a, b): boolean => {
@@ -10,8 +12,18 @@ const isWithinScareScaleRange = (a, b, range = 2): boolean =>
   Math.abs(a.scareScaleRating - b.scareScaleRating) <= range;
 
 const hasSimilarName = (a, b): boolean => {
-  const wordsA = new Set(a.data.name.toLowerCase().split(/\s+/));
-  const wordsB = new Set(b.data.name.toLowerCase().split(/\s+/));
+  
+  const getFilteredWords = (name: string): Set<string> =>
+    new Set(
+      name
+        .toLowerCase()
+        .split(/\s+/)
+        .filter((word) => word && !ignoredWords.has(word.toLowerCase()))
+    );
+
+  const wordsA = getFilteredWords(a.data.name);
+  const wordsB = getFilteredWords(b.data.name);
+
   return [...wordsA].some((word) => wordsB.has(word));
 };
 
